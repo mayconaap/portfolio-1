f037e47b682c5a2cdcb31d097a67d2906f08aed1
Maycon Pinto <128097751+mayconaap@users.noreply.github.com>
2024-06-30 12:42:35 -0300

    Add files via upload
    Animações utilizando biblioteca Scroll Reveal JS.

---
+++ reveal.js
@@ -0,0 +1,71 @@
+window.revelar = ScrollReveal({reset:true})
+
+// PROFILE  //
+
+revelar.reveal('.anima-pic-profile', {
+    duration: 800,
+    distance: '90px'
+})
+
+revelar.reveal('.anima-text-profile', {
+    duration: 800,
+    distance: '90px',
+    delay: 300
+})
+
+revelar.reveal('.anima-text', {
+    duration: 800,
+    distance: '90px'
+})
+
+// BARRA DE NAVEGACAO  //
+revelar.reveal('.efeito-nav', {
+    duration: 500,
+    distance: '150px',
+    origin:'left'
+
+})
+
+revelar.reveal('.efeito-nav1', {
+    duration: 500,
+    distance: '150px',
+    origin:'right'
+
+})
+
+// EXPERIÊNCIAS
+revelar.reveal('.anima-experience1', {
+    duration: 500,
+    distance: '150px',
+    origin:'left',
+    delay: 400
+})
+
+revelar.reveal('.anima-experience2', {
+    duration: 500,
+    distance: '150px',
+    origin:'right',
+    delay: 400
+
+})
+
+// PROJETOS
+revelar.reveal('.anima-projeto1', {
+    duration: 500,
+    distance: '350px',
+    origin:'right',
+    })
+
+revelar.reveal('.anima-projeto2', {
+    duration: 500,
+    distance: '350px',
+    origin:'right',
+    delay: 250
+    })
+
+revelar.reveal('.anima-projeto3', {
+    duration: 500,
+    distance: '350px',
+    origin:'right',
+    delay: 500
+    })
